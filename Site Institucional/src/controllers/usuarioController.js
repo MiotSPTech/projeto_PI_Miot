462b32a8d3f441922fa9dfb7ed0a8938984a0765
lualuaSantosS2 <luana.santos@sptech.school>
2024-05-24 15:02:16 -0300

    login subindo

--- Site Institucional/src/controllers/usuarioController.js
+++ Site Institucional/src/controllers/usuarioController.js
@@ -20,20 +20,21 @@ function autenticar(req, res) {
                     if (resultadoAutenticar.length == 1) {
                         console.log(resultadoAutenticar);
 
-                        aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
-                            .then((resultadoAquarios) => {
-                                if (resultadoAquarios.length > 0) {
+                        //aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
+                            //.then((resultadoAquarios) => {
+                                //if (resultadoAquarios.length > 0) {
                                     res.json({
-                                        id: resultadoAutenticar[0].id,
-                                        email: resultadoAutenticar[0].email,
-                                        nome: resultadoAutenticar[0].nome,
-                                        senha: resultadoAutenticar[0].senha,
-                                        aquarios: resultadoAquarios
+                                        idRepresentante: resultadoAutenticar[0].idRepresentante,
+                                        emailRepresentante: resultadoAutenticar[0].emailRepresentante,
+                                        nomeRepresentante: resultadoAutenticar[0].nomeRepresentante,
+                                        codigoIndustria: resultadoAutenticar[0].codigoIndustria,
+                                        cargoRepresentante: resultadoAutenticar[0].cargoRepresentante
+                                        //aquarios: resultadoAquarios
                                     });
-                                } else {
+                                //} else {
                                     res.status(204).json({ aquarios: [] });
-                                }
-                            })
+                                //}
+                            //})
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
                     } else {
